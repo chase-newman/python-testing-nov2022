a89c9943c2bdbb8be22a7072aedfe7b988eacc09
EC2 Default User <ec2-user@ip-172-31-86-13.ec2.internal>
2022-11-04 13:06:22 +0000

    second commit

--- hello.py
+++ hello.py
@@ -1,11 +1,27 @@
 import csv
+from csv import writer
 
-date = ["This", "is", "a", "Test"]
+
+def append_list_as_row(file_name, list_of_elem):
+     with open('securitieslist.csv', 'a+', newline='') as write_obj:
+         csv_writer = writer(write_obj)
+         csv_writer.writerow(list_of_elem)
+
+row_contents = ["Second Test", "COM", "112ABC44", "786", "11234", "SH", "Call", "Discretion", "Managers", "Sole", "Shared", "None"]
+
+x = 0
+
+for x in range(0,3):
+    append_list_as_row('securitieslist.csv', row_contents)
+    x + 1
 
 
-with open('example.csv','w') as file:
-    writer = csv.writer(file)
-    writer.writerow(date)
+# data = ["This", "is", "a", "Test"]
+
+
+# with open('example.csv','w') as file:
+#     writer = csv.writer(file)
+#     writer.writerow(data)
     
 
 # file = open('securities.csv')
